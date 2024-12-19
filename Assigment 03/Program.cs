044cee4e3cb2448dc145ea69cfc5cdebaa101cc2
Samer Zaki <Samer.Zaki22@svnu.edu.eg>
2024-12-19 10:48:48 +0200

    Full Assigment 03

--- Assigment 03/Program.cs
+++ Assigment 03/Program.cs
@@ -1,10 +1,227 @@
-﻿namespace Assigment_03
+﻿using System;
+
+namespace Assigment_03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 01
+            //int num=Convert.ToInt32(Console.ReadLine());
+            //if (num % 3 == 0 && num % 4 == 0)
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+            #endregion
+
+            #region Question 02
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if (num>0)
+            //{
+            //    Console.WriteLine("Positive");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Negative");
+            //}
+            #endregion
+
+            #region Question 03
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+            //if (num1 > num2 && num1 > num3 && num2 > num3)
+            //{
+            //    Console.WriteLine("Max Element= "+ num1);
+            //    Console.WriteLine("Min Element= "+ num3);
+            //}
+            //else if(num1 > num2 && num1 > num3 && num3 > num2)
+            //{
+            //    Console.WriteLine("Max Element= "+ num1);
+            //    Console.WriteLine("Min Element= "+ num2);
+            //}
+            //else if(num2 > num1 && num2 > num3 && num1 > num3)
+            //{
+            //    Console.WriteLine("Max Element= "+ num2);
+            //    Console.WriteLine("Min Element= "+ num3);
+            //}
+            //else if(num2 > num1 && num2 > num3 && num3 > num1)
+            //{
+            //    Console.WriteLine("Max Element= "+ num2);
+            //    Console.WriteLine("Min Element= "+ num1);
+            //}
+            //else if(num3 > num1 && num3 > num2 && num2 > num1)
+            //{
+            //    Console.WriteLine("Max Element= "+ num3);
+            //    Console.WriteLine("Min Element= "+ num1);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Max Element= "+ num3);
+            //    Console.WriteLine("Min Element= "+ num2);
+            //}
+            #endregion
+
+            #region Question 04
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if (num % 2 == 0)
+            //{
+            //    Console.WriteLine("Even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Odd");
+            //}
+            #endregion
+
+            #region Question 05
+            //char c = Convert.ToChar(Console.ReadLine());
+            //if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            //{
+            //    Console.WriteLine("vowel");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("consonant");
+            //}
+            #endregion
+
+            #region Question 07
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= 12; i++)
+            //{
+            //    Console.Write(num * i + " ");
+            //}
+            #endregion
+
+            #region Question 09
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(Math.Pow(num1, num2));
+            #endregion
+
+            #region Question 10
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+            //int num4 = Convert.ToInt32(Console.ReadLine());
+            //int num5 = Convert.ToInt32(Console.ReadLine());
+            //int total = num1 + num2 + num3 + num4 + num5;
+            //Console.WriteLine(total);
+            //Console.WriteLine(total / 5);
+            //Console.WriteLine((total / 500) * 100);
+            #endregion
+
+            #region Question 11
+            //int mon=Convert.ToInt32(Console.ReadLine());
+            //if (mon == 1 || mon == 3 || mon == 5 || mon == 7 || mon == 8 || mon == 10 || mon == 12)
+            //{
+            //    Console.WriteLine("Days in month: " + "31");
+            //}
+            //else if(mon == 2)
+            //{
+            //    Console.WriteLine("Days in month: " + "29");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Days in month: " + "30");
+            //}
+            #endregion
+
+            #region Question 17
+            //Console.WriteLine("Enter coordinates for three points:");
+
+            //Console.Write("x1: ");
+            //double x1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("y1: ");
+            //double y1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("x2: ");
+            //double x2 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("y2: ");
+            //double y2 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("x3: ");
+            //double x3 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("y3: ");
+            //double y3 = Convert.ToDouble(Console.ReadLine());
+
+            //double area = 0.5 * Math.Abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+
+            //if (area == 0)
+            //{
+            //    Console.WriteLine("The points are on the same line.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The points are not on the same line.");
+            //}
+            #endregion
+
+            #region Question 18
+            //double time = Convert.ToDouble(Console.ReadLine());
+
+            //if (time >= 2 && time <= 3)
+            //{
+            //    Console.WriteLine("The worker is highly efficient.");
+            //}
+            //else if (time > 3 && time <= 4)
+            //{
+            //    Console.WriteLine("The worker needs to increase their speed.");
+            //}
+            //else if (time > 4 && time <= 5)
+            //{
+            //    Console.WriteLine("The worker needs training to improve their speed.");
+            //}
+            //else if (time > 5)
+            //{
+            //    Console.WriteLine("The worker must leave the company.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Time should be 2 hours or more.");
+            //}
+            #endregion
+
+            #region Question 21
+            //int x = 10;
+            //Console.WriteLine(x);
+
+            //int y = x;
+            //Console.WriteLine(y);
+
+            //y = 20;
+            //Console.WriteLine(y);
+            #endregion
+
+            #region Question 22
+            //Point p1;
+            //p1 = new Point();
+
+            //Point p2;
+            //p2 = new Point();
+
+            //p2 = p1;
+
+            //object obj1 = new object();
+            //object obj2 = new object();
+            //obj1 = "Samer";
+            //obj2 = "Emad";
+
+            //obj2 = obj1;
+            //Console.WriteLine(obj2);
+            #endregion
+
+            #region Questions 23, 24, 25
+            //23->b
+            //24->d
+            //25->d
+            #endregion
+        
         }
     }
 }
